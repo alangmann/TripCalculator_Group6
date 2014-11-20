051a336a67a2d5f075e77280124c4f41d70f14f2
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2014-11-20 11:44:07 +0100

    add calculator class

---
+++ src/main/java/bl/Calculator.java
@@ -0,0 +1,11 @@
+package bl;
+
+/**
+ * Created by dominik on 20.11.2014.
+ */
+public class Calculator {
+    public double CO2_Consumption_Diesel;
+    public double CO2_Consumption_Petrol;
+
+
+}
