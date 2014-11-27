7ec3556eea2a3728df2e39f076954c8000e3a412
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2014-11-27 12:13:46 +0100

    test calculateCo2ConsumptionBasedOnDistance with Distance -1

--- src/test/java/TripCalculatorTest.java
+++ src/test/java/TripCalculatorTest.java
@@ -1,5 +1,10 @@
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import bl.Calculator;
+import bl.Route;
+import bl.Vehicle;
+import enums.FuelType;
+import enums.RouteType;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -13,4 +18,11 @@ public class TripCalculatorTest {
         assertTrue(true);
         System.out.println("lalala");
     }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testIfCalculateRouteWithDistanceMinusOneReturnsError() {
+        cal.calculateCo2ConsumptionBasedOnDistance(new Route(-1, 0,0, RouteType.HIGHWAY), new Vehicle(6, 1, FuelType.DIESEL));
+    }
+
+
 }
