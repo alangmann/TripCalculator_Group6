f7f0849f441fc719b0c221bd1fdcd522f6dc2e76
goevea11 <goevea11@htlkaindorf.at>
2014-11-27 12:12:15 +0100

    Auslesen aus CSV

---
+++ src/main/java/bl/RouteBL.java
@@ -0,0 +1,52 @@
+package bl;
+
+import enums.RouteType;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+/**
+ * Created by Veronika on 27.11.2014.
+ */
+public class RouteBL {
+    private LinkedList<Route> routes = new LinkedList<Route>();
+
+    public RouteBL(LinkedList<Route> routes) {
+        this.routes = routes;
+    }
+
+    public void loadRoute() throws IOException
+    {
+        String pfad = System.getProperty("user.dir") + File.separator
+                + "resources" + File.separator +"routes.csv";
+        BufferedReader br = new BufferedReader(new FileReader(new File(pfad)));
+        String line="";
+        try
+        {
+            while((line=br.readLine())!=null)
+            {
+                String[] splits = line.split(";");
+                //45;96,8;CountryRoad;0
+                // public Route(double distance, double slope, double specialFee, RouteType typeOfRoute) {
+                //km;slope;routtype;special fee
+                if (splits[2].equals("Highway")) {
+                    routes.add(new Route(Double.parseDouble(splits[0]), Double.parseDouble(splits[1]), Double.parseDouble(splits[3]), RouteType.HIGHWAY));
+                }
+                else if(splits[2].equals("CountryRoad"))
+                {
+                    routes.add(new Route(Double.parseDouble(splits[0]), Double.parseDouble(splits[1]), Double.parseDouble(splits[3]), RouteType.COUNTRYROAD));
+                }
+                else if(splits[2].equals("GravelRoad"))
+                {
+                    routes.add(new Route(Double.parseDouble(splits[0]), Double.parseDouble(splits[1]), Double.parseDouble(splits[3]), RouteType.GRAVELROAD));
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
