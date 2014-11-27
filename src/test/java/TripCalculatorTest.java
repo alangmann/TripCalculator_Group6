0ad5d5061544088122ecc6d51832a1fe4f1056b7
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2014-11-27 12:23:18 +0100

    add new test

--- src/test/java/TripCalculatorTest.java
+++ src/test/java/TripCalculatorTest.java
@@ -7,6 +7,7 @@ import enums.FuelType;
 import enums.RouteType;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class TripCalculatorTest {
@@ -21,7 +22,12 @@ public class TripCalculatorTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void testIfCalculateRouteWithDistanceMinusOneReturnsError() {
-        cal.calculateCo2ConsumptionBasedOnDistance(new Route(-1, 0,0, RouteType.HIGHWAY), new Vehicle(6, 1, FuelType.DIESEL));
+        cal.calculateCo2ConsumptionBasedOnDistance(new Route(-1, 0, 0, RouteType.HIGHWAY), new Vehicle(6, 1, FuelType.DIESEL));
+    }
+
+    @Test
+    public void testIfDieselWith10KmReturnsZeroKomma236() {
+        assertThat(this.cal.calculateCo2ConsumptionBasedOnDistance(new Route(10, 0, 0, RouteType.HIGHWAY), new Vehicle(6, 1, FuelType.DIESEL)), equalTo(0.236));
     }
 
 
