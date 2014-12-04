c26980940ceeff825a0cd2cefe29a7c1760c0e42
goevea11 <goevea11@htlkaindorf.at>
2014-12-04 12:13:01 +0100

    Getter und Setter Truck

--- src/main/java/bl/Truck.java
+++ src/main/java/bl/Truck.java
@@ -5,10 +5,27 @@ import enums.FuelType;
 /**
  * Created by Jule on 13.11.2014.
  */
-public class Truck extends Vehicle
+public class                                                                                                                                                                Truck extends Vehicle
 {
     private int axles;
     private boolean adBlue;
+
+    public boolean isAdBlue() {
+        return adBlue;
+    }
+
+    public void setAdBlue(boolean adBlue) {
+        this.adBlue = adBlue;
+    }
+
+    public int getAxles() {
+
+        return axles;
+    }
+
+    public void setAxles(int axles) {
+        this.axles = axles;
+    }
 
     public Truck(double averageConsumption, int cargo, FuelType typeOfFuel) {
         super(averageConsumption, cargo, typeOfFuel);
