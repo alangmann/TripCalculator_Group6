154ade8df24a241c200460b10e022474d7336cb5
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2014-12-11 11:26:39 +0100

    add test

--- src/test/java/TripCalculatorTest.java
+++ src/test/java/TripCalculatorTest.java
@@ -1,6 +1,7 @@
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import bl.Calculator;
+import bl.Car;
 import bl.Route;
 import bl.Vehicle;
 import enums.FuelType;
@@ -21,8 +22,12 @@ public class TripCalculatorTest {
     }
 
     @Test
-    public void testIfDieselWith10KmReturnsZeroKomma236() {
-        assertThat(this.cal.calculateCo2Consumption(new Route(10, 0, 0, RouteType.HIGHWAY), new Vehicle(6, 1, FuelType.DIESEL)), equalTo(0.236));
+    public void testIfDieselWithConsumption5l100kmonAMotorwayWithASlope5mon10KmReturns1Komma3256625() {
+        //If you drive your vehicle fueled with diesel with an average consumption of 5l/100km on a motorway with a slope of 5m on 10 km, you generate a CO2 output of 1,3256625kg.
+        Route route = new Route(10, 5, 1, RouteType.HIGHWAY);
+        Car car = new Car(5, 1, FuelType.DIESEL);
+
+        assertThat(this.cal.calculateCo2Consumption(route, car), equalTo(1.3256625));
     }
 
     @Test
