9438229129c6d57a616c7f5a026f141c4185a8ba
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2014-12-18 11:43:12 +0100

    add classes for sprit db

---
+++ src/main/java/enums/DayOfTheWeek.java
@@ -0,0 +1,14 @@
+package enums;
+
+/**
+ * Created by dominik on 18.12.2014.
+ */
+public enum DayOfTheWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
