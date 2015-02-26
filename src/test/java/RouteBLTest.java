d4808fabf529de0c4dc2a754d6267693df451929
samdoa11 <samdoa11@htl-kaindorf.ac.at>
2015-02-26 11:08:33 +0100

    spring

--- src/test/java/RouteBLTest.java
+++ src/test/java/RouteBLTest.java
@@ -7,10 +7,15 @@ import bl.Route;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
-import java.util.LinkedList;
 
+import java.util.LinkedList;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:resources/spring-di-sample-annotation-context.xml")
 public class RouteBLTest
 {
     private RouteBL m_RouteBL;
